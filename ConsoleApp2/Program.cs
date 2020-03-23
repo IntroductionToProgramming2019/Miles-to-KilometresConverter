a00f545ad21cf24feccafb82c3a7fed4ed3d8e33
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-23 17:28:50 +0000

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : Converting Miles to Kilometres
+ * ############################# */
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine("Please enter the number of miles: ");
+            int miles = int.Parse(Console.ReadLine());
+            double km = miles * 0.65;
+            Console.WriteLine(miles + " miles in kilometres is " + km);
+        }
+   
+    }
+}
